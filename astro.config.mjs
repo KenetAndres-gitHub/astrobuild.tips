366fc62507cd1aaafc620d96f5851a2d4fa27f6c
KenetAndres-gitHub <kenbok2014@hotmail.com>
2024-02-13 11:45:09 -0500

    MDX install y preparando las rutas dinámicas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
+//tailwinds es un framework de CSS basado en clases y no en componentes
+//mdx es una librería para poder agregar variables globales y usarlas en el proyecto
 
 // https://astro.build/config
 export default defineConfig({
