c857efe8214a943277537957a9586dedbded5be8
KenetAndres-gitHub <kenbok2014@hotmail.com>
2024-03-14 19:33:59 -0500

    Updated: add Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import vue from '@astrojs/vue';
 //tailwinds es un framework de CSS basado en clases y no en componentes
 //mdx es una librería para poder agregar variables globales y usarlas en el proyecto
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()]
+  integrations: [tailwind(), mdx(), react(), vue()],
 });
